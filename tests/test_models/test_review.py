af24a01ece6a5f728c28812daff183bb60336d0d
Godfred <goddysmart2468@gmail.com>
2023-12-11 14:35:02 -0800

    test suite for review class

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This defines the test suite for the Review class.
+"""
+
+import os
+import models
+import unittest
+from datetime import datetime
+from models.review import Review
+
+
+class TestReviewInstantiation(unittest.TestCase):
+    """
+    We are testing the instantiation of the Review class.
+    """
+    def test_create_instance(self):
+        review = Review()
+        self.assertIsInstance(review, Review)
+
+    def test_attributes(self):
+        review = Review()
+        self.assertEqual(review.place_id, "")
+        self.assertEqual(review.user_id, "")
+        self.assertEqual(review.text, "")
+
+
+if __name__ == "__main__":
+    unittest.main()
+
