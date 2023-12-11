15b11751a975523d3ed17755112934ccbaeed04d
Godfred <goddysmart2468@gmail.com>
2023-12-11 14:37:25 -0800

    test suite for state class

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+this defines the test suite for the State class.
+"""
+import unittest
+import os
+import models
+from models.state import State
+from datetime import datetime
+from models.review import Review
+
+
+class TestStateInstantiation(unittest.TestCase):
+    """
+    We are testing the instantiation of the State class.
+    """
+    def test_create_instance(self):
+        state = State()
+        self.assertIsInstance(state, State)
+
+    def test_attributes(self):
+        state = State()
+        self.assertEqual(state.name, "")
+
+
+if __name__ == "__main__":
+    unittest.main()
+
